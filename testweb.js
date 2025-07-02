cb0a59047a6cb10a82eee327ebf5145d9404bc81
risyaara <risyazahra1727@gmail.com>
2025-07-02 15:32:49 +0700

    Update testweb.js
    Signed-off-by: risyaara <risyazahra1727@gmail.com>

--- testweb.js
+++ testweb.js
@@ -28,7 +28,7 @@ document.getElementById("btnHitungDetail").addEventListener("click", function ()
         method: "POST",
      headers: {
             "Content-Type": "application/json"
-        }
+        },
         body: JSON.stringify({
             nama: namaTerpilih,
             namaKomponen: namaKomponen,
